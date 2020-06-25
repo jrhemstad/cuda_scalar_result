056af125da241fdcc63c202a3434b2de06dc00a3
Jake Hemstad <jhemstad@nvidia.com>
2020-06-25 10:32:26 -0500

    Add example using managed memory.

--- benchmarks/scalar/scalar_benchmark.cu
+++ benchmarks/scalar/scalar_benchmark.cu
@@ -27,24 +27,40 @@ __global__ void simple_reduction(std::size_t input_size, T *global_result) {
 }
 
 template <typename T> static void BM_device_memory(::benchmark::State &state) {
-
   auto size = state.range(0);
-
   T *d_result{};
-
+  T h_result{};
   cudaMalloc(&d_result, sizeof(T));
-
   constexpr std::size_t block_size{256};
-
   auto grid_size = (size + block_size + 1) / size;
 
   for (auto _ : state) {
     cudaMemset(d_result, 0, sizeof(T));
     simple_reduction<block_size><<<block_size, grid_size>>>(size, d_result);
-    cudaDeviceSynchronize();
+    cudaMemcpy(&h_result, d_result, sizeof(h_result), cudaMemcpyDefault);
   }
 }
 BENCHMARK_TEMPLATE(BM_device_memory, int)
+    ->RangeMultiplier(10)
+    ->Range(1'000'000, 1'000'000'000)
+    ->Unit(benchmark::kMillisecond);
+
+template <typename T> static void BM_managed_memory(::benchmark::State &state) {
+  auto size = state.range(0);
+  T *d_result{};
+  T h_result{};
+  cudaMallocManaged(&d_result, sizeof(T));
+  constexpr std::size_t block_size{256};
+  auto grid_size = (size + block_size + 1) / size;
+
+  for (auto _ : state) {
+    *d_result = 0;
+    simple_reduction<block_size><<<block_size, grid_size>>>(size, d_result);
+    cudaDeviceSynchronize();
+    benchmark::DoNotOptimize(h_result = *d_result);
+  }
+}
+BENCHMARK_TEMPLATE(BM_managed_memory, int)
     ->RangeMultiplier(10)
     ->Range(1'000'000, 1'000'000'000)
     ->Unit(benchmark::kMillisecond);
