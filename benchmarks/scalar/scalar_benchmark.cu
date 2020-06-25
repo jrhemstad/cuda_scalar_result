4356a3425e37b1032117e251aa14ef7702e3ffed
Jake Hemstad <jhemstad@nvidia.com>
2020-06-25 10:21:37 -0500

    Add initial reduction kernel to dev mem.

--- benchmarks/scalar/scalar_benchmark.cu
+++ benchmarks/scalar/scalar_benchmark.cu
@@ -1,44 +1,49 @@
 
 #include <benchmark/benchmark.h>
-
 #include <iostream>
+#include <cub/cub.cuh> 
+#include <thrust/iterator/iterator_traits.h>
 
-static void BM_test(::benchmark::State& state) {
+template <std::size_t block_size, typename T>
+__global__ void simple_reduction(std::size_t input_size, T *global_result) {
+
+  auto tid = threadIdx.x + blockIdx.x * gridDim.x;
+
+  T thread_data{};
+
+  while (tid < input_size) {
+    ++thread_data;
+    tid += blockDim.x * gridDim.x;
+  }
+
+  using BlockReduce = cub::BlockReduce<T, block_size>;
+
+  __shared__ typename BlockReduce::TempStorage temp_storage;
+
+  T block_result = BlockReduce(temp_storage).Sum(thread_data);
+
+  if (threadIdx.x == 0)
+    atomicAdd(global_result, block_result);
+}
+
+template <typename T> static void BM_device_memory(::benchmark::State &state) {
+
+  auto size = 1'000'000;
+
+  T *d_result{};
+
+  cudaMalloc(&d_result, sizeof(T));
+
+  constexpr std::size_t block_size{256};
+
+  auto grid_size = (size + block_size + 1) / size;
+
   for (auto _ : state) {
-      std::string string;
+    cudaMemset(d_result, 0, sizeof(T));
+    simple_reduction<block_size><<<block_size, grid_size>>>(size, d_result);
+    cudaDeviceSynchronize();
   }
 }
-BENCHMARK(BM_test);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state) std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
-
-static void BM_StringCompare(benchmark::State& state) {
-  std::string s1(state.range(0), '-');
-  std::string s2(state.range(0), '-');
-  for (auto _ : state) {
-    benchmark::DoNotOptimize(s1.compare(s2));
-  }
-  state.SetComplexityN(state.range(0));
-}
-BENCHMARK(BM_StringCompare)
+BENCHMARK_TEMPLATE(BM_device_memory, int)
     ->RangeMultiplier(2)
-    ->Range(1 << 10, 1 << 18)
-    ->Complexity(benchmark::oN);
-
-template <class Q>
-void BM_Sequential(benchmark::State& state) {
-  Q q;
-  typename Q::value_type v(0);
-  for (auto _ : state) {
-    for (int i = state.range(0); i--;) q.push_back(v);
-  }
-  // actually messages, not bytes:
-  state.SetBytesProcessed(static_cast<int64_t>(state.iterations()) *
-                          state.range(0));
-}
-BENCHMARK_TEMPLATE(BM_Sequential, std::vector<int>)->Range(1 << 0, 1 << 10);
+    ->Range(1 << 10, 1 << 18);
