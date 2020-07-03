1c655546708429a9f2bc5539b8d1bb82d4716d80
Jake Hemstad <jhemstad@nvidia.com>
2020-07-02 20:06:45 -0500

    Add results for managed + prefetch.

--- benchmarks/scalar/scalar_benchmark.cu
+++ benchmarks/scalar/scalar_benchmark.cu
@@ -67,6 +67,29 @@ BENCHMARK_TEMPLATE(BM_managed_memory, int)
     ->Range(100'000, 1'000'000'000)
     ->Unit(benchmark::kMillisecond);
 
+template <typename T>
+static void BM_managed_memory_prefetch(::benchmark::State &state) {
+  auto size = state.range(0);
+  T *d_result{};
+  T h_result{};
+  cudaMallocManaged(&d_result, sizeof(T));
+  constexpr std::size_t block_size{256};
+  auto grid_size = (size + block_size + 1) / size;
+
+  for (auto _ : state) {
+    *d_result = 0;
+    cudaMemPrefetchAsync(d_result, sizeof(T), 0);
+    simple_reduction<block_size><<<block_size, grid_size>>>(size, d_result);
+    cudaDeviceSynchronize();
+    benchmark::DoNotOptimize(h_result = *d_result);
+  }
+  cudaFree(d_result);
+}
+BENCHMARK_TEMPLATE(BM_managed_memory_prefetch, int)
+    ->RangeMultiplier(10)
+    ->Range(100'000, 1'000'000'000)
+    ->Unit(benchmark::kMillisecond);
+
 template <typename T> static void BM_pinned_memory(::benchmark::State &state) {
   auto size = state.range(0);
   T *d_result{};
