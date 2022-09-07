825a6c2f58cff2e9f8de2fdb15d85025b36685f3
Jake Hemstad <jhemstad@nvidia.com>
2022-09-07 18:34:08 -0500

    Port to use atomic_ref.

--- benchmarks/scalar/atomic_pinned.cu
+++ benchmarks/scalar/atomic_pinned.cu
@@ -3,76 +3,74 @@
 #include "kernels.cuh"
 
 #include <benchmark/benchmark.h>
-#include <cuda/std/atomic>
+#include <cuda/atomic>
 #include <new>
 
-template <typename T> __global__ void zero(T *count) { new (count) T{0}; }
-
-template <std::size_t block_size, typename T>
+template <std::size_t block_size>
 __global__ void std_reduction(
     std::size_t input_size,
-    cuda::atomic<T, cuda::thread_scope_system> *global_result,
-    cuda::atomic<T, cuda::thread_scope_device> *device_result,
-    cuda::atomic<unsigned int, cuda::thread_scope_device> *atomic_count) {
-  auto tid = threadIdx.x + blockIdx.x * gridDim.x;
-  T thread_data{};
-  while (tid < input_size) {
-    ++thread_data;
-    tid += blockDim.x * gridDim.x;
-  }
-  using BlockReduce = cub::BlockReduce<T, block_size>;
-  __shared__ typename BlockReduce::TempStorage temp_storage;
-  T block_result = BlockReduce(temp_storage).Sum(thread_data);
-  bool is_last_block_done = false;
-  if (threadIdx.x == 0) {
-    device_result->fetch_add(block_result, cuda::std::memory_order_relaxed);
-    unsigned value = atomic_count->fetch_add(1, cuda::memory_order_release);
-    is_last_block_done = value == (gridDim.x - 1);
-  }
-  if (is_last_block_done) {
-    // copy result to global buffer
-    if (threadIdx.x == 0) {
-      global_result->store(device_result->load(cuda::std::memory_order_relaxed), cuda::std::memory_order_relaxed);
-      device_result->store(0, cuda::std::memory_order_relaxed); // set to zero for next time
-      atomic_count->store(0, cuda::std::memory_order_relaxed); // set to zero for next time
-    }
-  }
+    int *global_result,
+    int *device_result,
+    int *count)
+{
+   auto tid = threadIdx.x + blockIdx.x * gridDim.x;
+   int thread_data{};
+   while (tid < input_size)
+   {
+      ++thread_data;
+      tid += blockDim.x * gridDim.x;
+   }
+   using BlockReduce = cub::BlockReduce<int, block_size>;
+   __shared__ typename BlockReduce::TempStorage temp_storage;
+   int block_result = BlockReduce(temp_storage).Sum(thread_data);
+   bool is_last_block_done = false;
+
+   cuda::atomic_ref<int, cuda::thread_scope_device> device_result_ref{*device_result};
+   cuda::atomic_ref<int, cuda::thread_scope_device> count_ref{*count};
+   if (threadIdx.x == 0)
+   {
+      device_result_ref.fetch_add(block_result, cuda::std::memory_order_relaxed);
+      unsigned value = count_ref.fetch_add(1, cuda::memory_order_release);
+      is_last_block_done = value == (gridDim.x - 1);
+      if (is_last_block_done)
+      {
+         // copy result to global buffer
+         cuda::atomic_ref<int, cuda::thread_scope_system> global_result_ref{*global_result};
+         global_result_ref.store(device_result_ref.load(cuda::std::memory_order_relaxed), cuda::std::memory_order_relaxed);
+         
+         
+         device_result_ref.store(0, cuda::std::memory_order_relaxed); // set to zero for next timek
+         count_ref.store(0, cuda::std::memory_order_relaxed);  // set to zero for next time
+      }
+   }
 }
 
-
 template <typename T>
-static void BM_std_pinned_memory(::benchmark::State &state) {
+static void BM_std_pinned_memory(::benchmark::State &state)
+{
+   auto size = state.range(0);
 
-  auto size = state.range(0);
-  using count_t = cuda::atomic<unsigned int, cuda::thread_scope_device>;
-  count_t *atomic_count;
-  cudaMalloc(&atomic_count, sizeof(count_t));
-  zero<<<1, 1>>>(atomic_count);
-  using device_atomic = cuda::atomic<T, cuda::thread_scope_device>;
-  device_atomic *d_result{};
-  cudaMalloc(&d_result, sizeof(device_atomic));
-  zero<<<1, 1>>>(d_result);
-  using system_atomic = cuda::atomic<T, cuda::thread_scope_system>;
-  system_atomic *hd_result{};
-  cudaMallocHost(&hd_result, sizeof(system_atomic));
-  T h_result{};
-  constexpr std::size_t block_size{256};
-  auto grid_size = (size + block_size + 1) / size;
-  for (auto _ : state) {
-    *hd_result = 0;
-    std_reduction<block_size>
-        <<<block_size, grid_size>>>(size, hd_result, d_result, atomic_count);
-    while (!hd_result->load(cuda::memory_order_acquire))
-      ;
-    benchmark::DoNotOptimize(h_result = *hd_result);
-  }
-  cudaFreeHost(hd_result);
-  cudaFree(d_result);
-  cudaFree(atomic_count);
+   int *storage;
+   cudaMalloc(&storage, 2 * sizeof(int));
+
+   int *hd_result{};
+   cudaMallocHost(&hd_result, sizeof(int));
+   T h_result{};
+   constexpr std::size_t block_size{256};
+   auto grid_size = (size + block_size + 1) / size;
+
+   for (auto _ : state)
+   {
+      *hd_result = 0;
+      std_reduction<block_size><<<block_size, grid_size>>>(size, hd_result, &storage[0], &storage[1]);
+      cuda::atomic_ref<int, cuda::thread_scope_system> hd_result_ref{*hd_result};
+      while (hd_result_ref.load(cuda::memory_order_acquire) == 0);
+      benchmark::DoNotOptimize(h_result = *hd_result);
+   }
+
+   cudaFreeHost(hd_result);
+   cudaFree(storage);
 }
 BENCHMARK_TEMPLATE(BM_std_pinned_memory, int)
     ->Apply(generate_size)
     ->Unit(benchmark::kMicrosecond);
-
-
-
